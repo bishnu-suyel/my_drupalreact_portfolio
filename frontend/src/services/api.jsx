457aa97ceb93b497f2c84f50bdf3f72420d5b72f
Bishnu Suyel <suyel2007@gmail.com>
2025-01-20 22:39:39 +0200

    Update fetchContent function with include fields

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,8 +1,10 @@
+// services/api.js
 import axios from 'axios';
 
 const API_URL = "http://my-drupal-portfolio.lndo.site/jsonapi/";
 
-export const fetchContent = async (contentType) => {
-  const response = await axios.get(`${API_URL}/${contentType}`);
+export const fetchContent = async (contentType, includeFields = []) => {
+  const includeParam = includeFields.length > 0 ? `?include=${includeFields.join(',')}` : '';
+  const response = await axios.get(`${API_URL}${contentType}${includeParam}`);
   return response.data;
 };
